0eb0cc46441ab6ce70af4f66dd8a327811048eb9
helmibenzina101 <helmi.benzina@gmail.com>
2023-02-23 12:21:41 +0100

    final project fruits angularjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CreateComponent } from './fruits/create/create.component';
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'fruits/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'fruits/create',
+    component: CreateComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
